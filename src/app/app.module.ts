75382fe433b369e6e8506bbc513c9c994eb2b57d
Constantin <cirimia100@gmail.com>
2022-02-07 15:30:33 -0600

    updated files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule, Routes } from "@angular/router";
+import { AppComponent } from "./app.component";
+import { ProductListComponent } from "./components/product-list/product-list.component";
+import { ProductService } from "./services/product.service";
 
-import { AppComponent } from './app.component';
-import { ProductListComponent } from './components/product-list/product-list.component';
-import { HttpClientModule } from '@angular/common/http';
-import { ProductService } from './services/product.service';
+const routes: Routes = [
+  { path: "category/:id", component: ProductListComponent },
+  { path: "category", component: ProductListComponent },
+  { path: "products", component: ProductListComponent },
+  { path: "", redirectTo: "/products", pathMatch: "full" },
+  { path: "**", redirectTo: "/products", pathMatch: "full" },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductListComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule
-  ],
+  declarations: [AppComponent, ProductListComponent],
+  imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(routes)],
   providers: [ProductService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,28 +1,46 @@
-import { Component, OnInit } from '@angular/core';
-import { ProductService } from 'src/app/services/product.service';
-import { Product } from 'src/app/common/product';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Product } from "src/app/common/product";
+import { ProductService } from "src/app/services/product.service";
 
 @Component({
-  selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  selector: "app-product-list",
+  templateUrl: "./product-list-grid.component.html",
+
+  styleUrls: ["./product-list.component.css"],
 })
 export class ProductListComponent implements OnInit {
-
   products: Product[];
-  
-  constructor(private productService: ProductService) { }
+  currentCategoryId: number;
+
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
-    this.listProducts();
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    });
   }
+
+  // check if if param is available
 
   listProducts() {
-    this.productService.getProductList().subscribe(
-      data => {
-        this.products = data;
-      }
-    )
-  }
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has("id");
+    if (hasCategoryId) {
+      // read and convert to number
+      this.currentCategoryId = +this.route.snapshot.paramMap.get("id");
+    } else {
+      this.currentCategoryId = 1;
+    }
 
+    //get the products
+
+    this.productService
+      .getProductList(this.currentCategoryId)
+      .subscribe((data) => {
+        this.products = data;
+      });
+  }
 }
